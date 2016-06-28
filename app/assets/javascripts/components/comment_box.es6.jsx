aff79c7504c48707352603cb661e0c4baf833136
Kimihiko NAKAMURA <nakamura.808@gmail.com>
2016-06-28 19:31:15 +0900

    children components

--- app/assets/javascripts/components/comment_box.es6.jsx
+++ app/assets/javascripts/components/comment_box.es6.jsx
@@ -2,7 +2,9 @@ class CommentBox extends React.Component {
   render () {
     return (
       <div className="commentBox">
-        Hello, world! I am a CommentBox.
+        <h1>Comments</h1>
+        <CommentList />
+        <CommentForm />
       </div>
     );
   }
---
+++ app/assets/javascripts/components/comment_form.es6.jsx
@@ -0,0 +1,9 @@
+class CommentForm extends React.Component {
+  render () {
+    return (
+      <div className="commentForm">
+        Hello, world! I am a CommentForm.
+      </div>
+    );
+  }
+}
