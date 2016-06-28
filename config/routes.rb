8bc47929ab77304c5c89cad77139c9f078d94684
Kimihiko NAKAMURA <nakamura.808@gmail.com>
2016-06-28 18:47:17 +0900

    Create CommentsController, set route to index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'static#index'
+  root 'comments#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
